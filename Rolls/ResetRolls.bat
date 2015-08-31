d14c2065a7bdfa7af206bb3dcfb54a1ae9ef3883
Amund211 <amund@svandal.no>
2015-08-31 20:28:18 +0200

    Create ResetRolls.bat

---
+++ Rolls/ResetRolls.bat
@@ -0,0 +1,14 @@
+@echo off
+for /f "delims=" %%a in ('cd') do @set dir=%%a
+del %dir%\6.txt
+del %dir%\5.txt
+del %dir%\4.txt
+del %dir%\3.txt
+del %dir%\2.txt
+del %dir%\1.txt
+echo. > %dir%\6.txt
+echo. > %dir%\5.txt
+echo. > %dir%\4.txt
+echo. > %dir%\3.txt
+echo. > %dir%\2.txt
+echo. > %dir%\1.txt
